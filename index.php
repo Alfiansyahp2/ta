278df0099145d4f2fde76bba91657e79b39f75da
Meynurafni <138767313+Meynurafni@users.noreply.github.com>
2023-10-31 09:36:53 +0700

    menambahkan nav link home di index.php

--- index.php
+++ index.php
@@ -75,6 +75,9 @@
 			<li class="nav-item">
               <a class="nav-link" href="logout.php">Logout</a>
             </li>
+			<li class="nav-item">
+              <a class="nav-link" href="index.php">Home</a>
+            </li>
 			<?php
 				}else{
 			?>
@@ -83,6 +86,10 @@
             </li>
             <li class="nav-item">
 				<a class="nav-link" href="?page=register">Register</a>
+            </li>
+			<li class="nav-item">
+				<a class="nav-link" href="?page=home">Home</a>
+				
             </li>
 			<?php
 				}
