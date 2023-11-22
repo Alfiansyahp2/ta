d86c8a8cf9ed82924ebc7efa4010ffbf2d87ef9c
Meynurafni <138767313+Meynurafni@users.noreply.github.com>
2023-11-22 17:05:38 +0700

    menambah redirect pada donat.php

--- donat.php
+++ donat.php
@@ -122,18 +122,18 @@ if ($page == "keranjang") {
 
 	<div class='row'>
     <?php
-    $querydonat = mysqli_query($mysqli, "SELECT kategori.kategori, kue.id_kue, kue.nama_kue, kue.spesifikasi, kue.gambar, kue.harga FROM kategori RIGHT JOIN kue ON kategori.id_kategori = kue.id_kategori WHERE kategori='donat' AND kue.status='on' ORDER BY RAND() ") or die(mysqli_error($mysqli));
-    while ($rowdonat = mysqli_fetch_assoc($querydonat)) {
+    $querydonat = mysqli_query($mysqli, "SELECT kategori.kategori, kue.id_kue, kue.nama_kue, kue.spesifikasi, kue.gambar, kue.harga FROM kategori RIGHT JOIN kue ON kategori.id_kategori = kue.id_kategori WHERE kategori='donat' AND kue.status='on' ") or die(mysqli_error($mysqli));
+    while ($rowkatalog = mysqli_fetch_assoc($querydonat)) {
         echo "<div class='col-lg-3 col-md-6 mb-4'>
             <div class='card h-100'>
-                <img class='card-img-top' src='images/kue/{$rowdonat['gambar']}' alt='gambar'>
+                <img class='card-img-top' src='images/kue/{$rowkatalog['gambar']}' alt='gambar'>
                 <div class='card-body'>
-                    <h4 class='card-title' style='color: blue;'>{$rowkdonat['nama_kue']}</h4>
-                    <h5>" . rupiah($rowdonat['harga']) . "</h5>
-                    <p class='card-text'>{$rowdonat['spesifikasi']}</p>
+                    <h4 class='card-title' style='color: blue;'>{$rowkatalog['nama_kue']}</h4>
+                    <h5>" . rupiah($rowkatalog['harga']) . "</h5>
+                    <p class='card-text'>{$rowkatalog['spesifikasi']}</p>
                 </div>
                 <div class='card-footer'>
-                    <small class='text-muted'><a href='tambah_keranjang.php?id_kue={$rowdonat['id_kue']}' class='btn btn-danger'>+Masukan Keranjang</a></small>
+                    <small class='text-muted'><a href='tambah_keranjang.php?id_kue={$rowkatalog['id_kue']}&redirect=donat.php' class='btn btn-danger'>+Masukan Keranjang</a></small>
                 </div>
             </div>
         </div>";
