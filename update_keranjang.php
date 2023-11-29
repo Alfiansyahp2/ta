670edc5d512458cdb4dc545bb66e3f711d1f1866
Meynurafni <138767313+Meynurafni@users.noreply.github.com>
2023-11-29 09:19:15 +0700

    mengubah fungsi pada produk.php dan update_keranjang.php

--- update_keranjang.php
+++ update_keranjang.php
@@ -2,12 +2,27 @@
 
 	session_start();
 	
+	if (!isset($_SESSION["keranjang"])) {
+		$_SESSION["keranjang"] = array();
+	}
+
 	$keranjang = $_SESSION["keranjang"];
 	$id_kue = $_POST["id_kue"];
 	$value = $_POST["value"];
 	
-	$keranjang[$id_kue]["qty"] = $value;
-	
+	if (!is_numeric($value)) {
+		exit;
+	}
+
+	if($value > 0){
+		$keranjang[$id_kue]["qty"] = $value;
+	}else{
+		if (isset($keranjang[$id_kue])) {
+			unset($keranjang[$id_kue]);
+		}
+	}
+
 	$_SESSION["keranjang"] = $keranjang;
 
+	var_dump($keranjang, $id_kue, $value);
 ?>
