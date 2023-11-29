6d0061aaaa9b1696efe290aacdb6f1f742ebe95f
Meynurafni <138767313+Meynurafni@users.noreply.github.com>
2023-11-29 14:33:02 +0700

    menambah choose file untuk upload bukti pembayaran pada file konfirmasi_pembayaran.php

--- module/pesanan/konfirmasi_pembayaran.php
+++ module/pesanan/konfirmasi_pembayaran.php
@@ -1,30 +1,37 @@
 <?php
-	
-	$id_pesanan = $_GET["id_pesanan"];
+
+$id_pesanan = $_GET["id_pesanan"];
 
 ?>
 
 <div class="col-lg-6">
-    
-        <div class="card-title">
-            <h4>Form Konfirmasi Pembayaran</h4>
-        </div>
-		
-            <div class="card-body">
-                <div class="basic-form">
-                    <form action="module/pesanan/proses_konfirmasi.php?id_pesanan=<?php echo $id_pesanan; ?>" method="POST">
-                        <div class="form-group">
-                            <label>Nomor rekening</label>
-                            <input type="text" name="norek" class="form-control" placeholder="Nomor Rekening" required>
-                        </div>
-						<div class="form-group">
-                            <label>Nama Akun</label>
-                            <input type="text" name="nama_akun" class="form-control" placeholder="Nama Akun" required>
-                        </div>
-                            <input type="submit" name="kirim" value="Kirim" class="btn btn-success">
-                    </form>
-					
+
+    <div class="card-title">
+        <h4>Form Konfirmasi Pembayaran</h4>
+    </div>
+
+    <div class="card-body">
+        <div class="basic-form">
+            <form action="module/pesanan/proses_konfirmasi.php?id_pesanan=<?php echo $id_pesanan; ?>" method="POST" enctype="multipart/form-data">
+                <div class="form-group">
+                    <label>Nomor rekening</label>
+                    <input type="text" name="norek" class="form-control" placeholder="Nomor Rekening" required>
                 </div>
-            </div>
-        
+                <div class="form-group">
+                    <label>Nama Akun</label>
+                    <input type="text" name="nama_akun" class="form-control" placeholder="Nama Akun" required>
+                </div>
+                <div class="form-group">
+                    <label>Unggah Bukti Pembayaran</label>
+                    <input type="file" name="bukti_pembayaran" accept=".pdf, .jpg, .png" required>
+                </div>
+                <input type="submit" name="kirim" value="Kirim" class="btn btn-success">
+            </form>
+        </div>
+    </div>
+
+
+</div>
+</div>
+
 </div>
