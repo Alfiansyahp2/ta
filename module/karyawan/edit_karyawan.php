e0b8c609f0f4e30b51380349406d3aaabf30a6e3
Altra-read <144076206+Altra-read@users.noreply.github.com>
2023-12-03 06:31:04 +0700

    pembenaran kode pada karyawan

--- module/karyawan/edit_karyawan.php
+++ module/karyawan/edit_karyawan.php
@@ -1,67 +1,23 @@
 <?php
-include "../../koneksi.php";
+$id_karyawan = @$_GET['id_karyawan'];
+$query = "SELECT * FROM karyawan WHERE id_karyawan='$id_karyawan'";
+$result = $mysqli->query($query);
 
-// Inisialisasi variabel $status agar tidak terjadi error jika tidak ada data
-$status = "";
-
-if ($_SERVER["REQUEST_METHOD"] === "GET") {
-    $id_karyawan = isset($_GET['id_karyawan']) ? $_GET['id_karyawan'] : null;
-
-    if ($id_karyawan) {
-        $sql = $mysqli->query("SELECT * FROM karyawan WHERE id_karyawan='$id_karyawan'");
-
-        if ($sql) {
-            $data = $sql->fetch_assoc();
-            // Periksa apakah data status ada sebelum mencoba mengaksesnya
-            $status = isset($data['status']) ? $data['status'] : "";
-        } else {
-            die("MySQLi error: " . $mysqli->error);
-        }
-    }
-}
-
-if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['edit_karyawan'])) {
-    $id_karyawan = isset($_POST['id_karyawan']) ? $_POST['id_karyawan'] : null;
-    
-    if ($id_karyawan) {
-        $nama = $_POST['nama_karyawan'];
-        $no_telp = $_POST['no_telp'];
-        $alamat = $_POST['alamat'];
-        $email = $_POST['email'];
-        $jabatan = $_POST['jabatan'];
-        $status = $_POST['status'];
-
-        // Gunakan parameterized query untuk menghindari SQL injection
-        $updateQuery = $mysqli->prepare("UPDATE karyawan SET nama_karyawan=?, no_telp=?, alamat=?, email=?, jabatan=?, status=? WHERE id_karyawan=?");
-
-        // Bind the parameters
-        $updateQuery->bind_param("sssssi", $nama, $no_telp, $alamat, $email, $jabatan, $status, $id_karyawan);
-
-        if ($updateQuery->execute()) {
-            // Redirect after successful form submission
-            header("Location: ../ta/halaman_admin.php?page=karyawan");
-            exit();
-        } else {
-            $error_message = "Update karyawan failed: " . $mysqli->error;
-        }
-    }
+if ($result) {
+    $data = $result->fetch_assoc();
+    $status = $data['status'];
+} else {
+    die("Gagal menjalankan query: " . $mysqli->error);
 }
 ?>
 
-
-<!-- The HTML form goes here -->
 <div class="col-lg-8">
     <div class="card-title">
-        <h4>Form Edit Karyawan</h4>
+        <h4>Form Edit User</h4>
     </div>
     <div class="card-body">
         <div class="basic-form">
             <form action="" method="post">
-                <!-- Add a hidden input field to capture id_karyawan -->
-                <input type="hidden" name="id_karyawan" value="<?php echo $id_karyawan; ?>">
-
-                <!-- ... (form fields lainnya) ... -->
-
                 <div class="form-group">
                     <label>Nama Karyawan</label>
                     <input type="text" name="nama_karyawan" class="form-control" value="<?php echo $data['nama_karyawan']; ?>" required>
@@ -89,9 +45,40 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['edit_karyawan'])) {
                         <input type="radio" name="status" id="opsi2" value="off" <?php if($status == "off"){ echo "checked='true'"; } ?> required> Off
                     </div>
                 </div>
-
                 <input type="submit" name="edit_karyawan" value="Edit" class="btn btn-default">
             </form>
         </div>
     </div>
 </div>
+
+<?php
+if (isset($_POST['edit_karyawan'])) {
+    $nama = $_POST['nama_karyawan'];
+    $no_telp = $_POST['no_telp'];
+    $alamat = $_POST['alamat'];
+    $email = $_POST['email'];
+    $jabatan = $_POST['jabatan'];
+    $status = $_POST['status'];
+
+    $updateQuery = $mysqli->prepare("UPDATE karyawan SET nama_karyawan=?, no_telp=?, alamat=?, email=?, jabatan=?, status=? WHERE id_karyawan=?");
+
+    // Bind the parameters
+    $updateQuery->bind_param("ssssssi", $nama, $no_telp, $alamat, $email, $jabatan, $status, $id_karyawan);
+
+    if ($updateQuery->execute()) {
+        ?>
+        <script type="text/javascript">
+            alert("Data user berhasil diedit");
+            window.location.href="../ta/halaman_admin.php?page=karyawan";
+        </script>
+        <?php
+    } else {
+        ?>
+        <script type="text/javascript">
+            alert("Update user failed: <?php echo $mysqli->error; ?>");
+        </script>
+        <?php
+    }
+    $updateQuery->close();
+}
+?>
