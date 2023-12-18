84543e1233a0f8e0802c350d175ec3e14da2f1c6
Altra-read <144076206+Altra-read@users.noreply.github.com>
2023-12-18 07:09:11 +0700

    pemeberian link power bi yang baru

--- home_admin.php
+++ home_admin.php
@@ -1,21 +1,16 @@
-<div class="card-title" style="text-align: center;">
-            <br><h1>Selamat Datang di Halaman Admin</h1>
-        </div>
-        <br>
 <div class="row">
           <div class="col-lg-12">
-            <div class="card">
-              <div class="card-body">
+            <div class="card-body" style="text-align: center;">
+                <h1>Selamat Datang di Halaman Admin</h1>
                 <div class="panel-group">
                   <div class="panel panel-default">
                     <div class="panel-body">
                       <div class="embed-responsive embed-responsive-16by9">
-                        <iframe class="embed-responsive-item" title="Report Section" src="https://app.powerbi.com/view?r=eyJrIjoiZWQwYzAwZmQtYmQxOS00ZGRjLWIwYWEtM2IxYjQ5NzdmZDU2IiwidCI6IjUyNjNjYzgxLTU5MTItNDJjNC1hYmMxLWQwZjFiNjY4YjUzMCIsImMiOjEwfQ%3D%3D" frameborder="1" allowfullscreen="True"></iframe>
+                        <iframe class="embed-responsive-item" title="Report Section" src="https://app.powerbi.com/view?r=eyJrIjoiOTQ5ZTM5OTgtODA2Ny00ZDEwLTk5NDgtNDc5YzM2ODlhOGZjIiwidCI6IjUyNjNjYzgxLTU5MTItNDJjNC1hYmMxLWQwZjFiNjY4YjUzMCIsImMiOjEwfQ%3D%3D" frameborder="1" allowfullscreen="True"></iframe>
                       </div>
                     </div>
                   </div>
                 </div>
-              </div>
             </div>
           </div>
-  </div>
+</div>
